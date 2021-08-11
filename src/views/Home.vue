adba5df582673cbbb55333d483199f0682ace253
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-08-11 12:18:47 +0200

    showing projects data using fetch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    <p>Hello Home</p>
-  </div>
+  <section>
+    <div v-if="projects.length" class="projects">
+      <div v-for="project in projects" :key="project.id">
+        <h3 class="projects__title">{{ project.title }}</h3>
+        <p class="projects__details">{{ project.details }}</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -9,5 +14,16 @@
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>

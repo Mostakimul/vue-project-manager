6e9d1722dfdbb752ca966a0234e45c16d2e71e37
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-08-12 11:43:33 +0200

    rounting link added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
+  <TheNavbar />
   <router-view />
 </template>
+<script>
+import TheNavbar from '@/components/TheNavbar.vue';
 
+export default {
+  components: {
+    TheNavbar,
+  },
+};
+</script>
 <style lang="scss">
 #app {
   max-width: 60rem;
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="nav">
+    <router-link :to="{ name: 'Home' }">Projects</router-link>
+    <router-link :to="{ name: 'AddNewProject' }">Add new project</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'TheNavbar',
+};
+</script>
+
+<style lang="scss">
+.nav {
+  text-align: center;
+  margin: 4rem auto;
+
+  & a {
+    display: inline-block;
+    text-decoration: none;
+    margin: 0 1rem;
+    color: #999;
+    font-size: 1.8rem;
+  }
+}
+a.router-link-active {
+  border-bottom: 2px solid crimson;
+  padding-bottom: 0.4rem;
+}
+</style>
